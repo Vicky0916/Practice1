e5c4a1eecff540bcffa1dba4f058564c63b8590a
Vickey0916 <271982953@qq.com>
2019-11-05 21:15:10 +0800

    commit

---
+++ Delete/Delete/Delete.cpp
@@ -0,0 +1,17 @@
+class Solution
+{
+public:
+	int removeElement(vector<int>& nums, int val)
+	{
+		int i = 0;
+		int j = 0;
+		for (i = 0; i < nums.size(); i++)
+		{
+			if (nums[i] != val)
+			{
+				nums[j++] = nums[i];
+			}
+		}
+		return j;
+	}
+};
