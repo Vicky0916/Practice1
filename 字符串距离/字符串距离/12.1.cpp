e6c701c74d9a6881032922bcb4a01dc131658b2e
Vickey0916 <271982953@qq.com>
2019-12-02 22:03:45 +0800

    commit

---
+++ 字符串距离/字符串距离/12.1.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <stdlib.h>
+#include <string>
+using namespace std;
+
+int main()
+{
+	string str1;
+	string str2;
+	cin >> str1 >> str2;
+	int step = 0;
+	//if (str1.size() < str2.size())
+	//{
+	//	swap(str1, str2);
+	//}
+	//for (int i = 0; i < str1.size(); i++)
+	//{
+	//	//错位了
+	//	if (str1[i] != str2[i] && str1[i + 1] == str2[i])
+	//	{
+	//		str2 = str1[i] + str2;
+	//		step++;
+	//	}
+	//	//没有错位，但是两个值不相等,替换
+	//	if (str1[i] != str2[i])
+	//	{
+	//		str2[i] = str1[i];
+	//		step++;
+	//	}
+	//}
+	if()
+	cout << step << endl;
+	system("pause");
+	return 0;
+}
