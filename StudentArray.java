1ce4d04c6e775fa1aadc842ac3e5914bf9411c88
Vickey0916 <271982953@qq.com>
2019-12-22 20:32:01 +0800

    程蕊 commit

---
+++ StudentArray.java
@@ -0,0 +1,19 @@
+package pakage1;
+
+public class StudentArray 
+{
+	public int sno;
+	public String name;
+	public StudentArray() 
+	{
+		// TODO Auto-generated constructor stub
+		
+	}
+	public StudentArray(int sno,String name)
+	{
+		this.sno=sno;
+		this.name=name;
+	}
+	
+
+}
