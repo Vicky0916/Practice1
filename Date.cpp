7d1dd551f3fe1f66d71beac246539fd856e011ee
Vickey0916 <271982953@qq.com>
2020-01-16 22:42:06 +0800

    程蕊 ommit

---
+++ Date.cpp
@@ -0,0 +1,35 @@
+
+#include "Date.h"
+
+Date& operator < (const Date& d)
+{
+	if (d._year < )
+		return 0;
+	else if
+	{
+       
+	}
+}
+bool operator > (const Date& d)
+{
+
+}
+bool operator<=(const Date& d)
+{
+
+}
+bool operator>=(const Date& d)
+{
+
+}
+Date& operator==(const Date& d)
+{
+	_year = d._year;
+	_month = d._month;
+	_day = d._day;
+	return *this;
+}
+bool operator!=(const Date& d)
+{
+
+}
