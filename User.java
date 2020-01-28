a35cc55ac5c4b05b74d78bc6e385c9eaf64e1cac
Vickey0916 <271982953@qq.com>
2020-01-28 23:39:01 +0800

    commit

---
+++ User.java
@@ -0,0 +1,34 @@
+package com.model;
+
+public class User {
+	private int u_id;
+	private String u_LoginName;
+	private String u_realName;
+	private String u_pwd;
+	public int getU_id() {
+		return u_id;
+	}
+	public void setU_id(int u_id) {
+		this.u_id = u_id;
+	}
+	public String getU_LoginName() {
+		return u_LoginName;
+	}
+	public void setU_LoginName(String u_LoginName) {
+		this.u_LoginName = u_LoginName;
+	}
+	public String getU_realName() {
+		return u_realName;
+	}
+	public void setU_realName(String u_realName) {
+		this.u_realName = u_realName;
+	}
+	public String getU_pwd() {
+		return u_pwd;
+	}
+	public void setU_pwd(String u_pwd) {
+		this.u_pwd = u_pwd;
+	}
+	
+
+}
