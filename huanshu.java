22977b9c5fbd89ce25f6c061cc4dfc792fc116bf
Vickey0916 <271982953@qq.com>
2020-02-18 20:36:03 +0800

    commit

---
+++ huanshu.java
@@ -0,0 +1,34 @@
+package com.dao;
+
+public class huanshu {
+	private String Rea_no;
+	private String book_no;
+	private String Ret_date;
+	private String Ret_fine;
+	public String getRea_no() {
+		return Rea_no;
+	}
+	public void setRea_no(String rea_no) {
+		this.Rea_no = rea_no;
+	}
+	public String getBook_no() {
+		return book_no;
+	}
+	public void setBook_no(String book_no) {
+		this.book_no = book_no;
+	}
+	public String getRet_date() {
+		return Ret_date;
+	}
+	public void setRet_date(String ret_date) {
+		this.Ret_date = ret_date;
+	}
+	public String getRet_fine() {
+		return Ret_fine;
+	}
+	public void setRet_fine(String ret_fine) {
+		this.Ret_fine = ret_fine;
+	}
+	
+
+}
